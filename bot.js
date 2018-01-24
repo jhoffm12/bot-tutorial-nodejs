b392eebc2a30cb75355c69228be6811ec7e944cb
jacksonhoffmann <jacksonhoffmann19@gmail.com>
2018-01-23 19:37:18 -0500

    Added request logging statement

--- bot.js
+++ bot.js
@@ -7,6 +7,8 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/hot take$/;
 
+  console.log(request);
+
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
