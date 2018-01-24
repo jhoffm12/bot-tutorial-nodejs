654c62b4d63b4ece9bdad102740f0b2cdb4dc300
jacksonhoffmann <jacksonhoffmann19@gmail.com>
2018-01-23 20:14:11 -0500

    Added logging

--- bot.js
+++ bot.js
@@ -7,6 +7,9 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/hot take$/,
       hotTakePrefix = "Hot take:";
+  
+  console.log("Name:" + request.name);
+  console.log(request.text);
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
